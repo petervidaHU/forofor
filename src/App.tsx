702cf3bd4ce905e2f6b6a06d1a326ddc59ec0561
peter_vida <csandragupta@gmail.com>
2024-09-15 00:55:13 +0200

    stylex config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import * as x from '@stylexjs/stylex';
+
+const s = x.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    backgroundColor: 'red',
+  },
+  logo: {
+    height: '6em',
+    padding: '1.5em',
+    willChange: 'filter',
+    transition: 'filter 300ms',
+  },
+  logoHover: {
+    filter: 'drop-shadow(0 0 2em #646cffaa)',
+  }
+});
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div {...x.props(s.container)}>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -19,7 +40,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+         fffffff count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
